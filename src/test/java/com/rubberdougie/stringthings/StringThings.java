78a3446dc35e949d44dc1c2c71cfc988a442742a
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-04 20:58:43 -0500

    Touched up for extendability

--- src/test/java/com/rubberdougie/stringthings/StringThings.java
+++ src/test/java/com/rubberdougie/stringthings/StringThings.java
@@ -5,17 +5,40 @@ import java.util.List;
 
 public class StringThings {
 
+	static final List<Character> DIGITS = new ArrayList<>(List.of('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
+
+	static final List<Character> ROMAN_LETTERS = new ArrayList<>(
+			List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+					'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+					'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+
 	public static String removeEndDigits(String string) {
 		int originalStrLen = string.length();
 		int newStrLen;
+
+		// null string isn't handled
 
 		if (originalStrLen == 0) {
 			return "";
 		}
 
-		List<Character> digits = new ArrayList<>(List.of('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'));
+		for (newStrLen = originalStrLen - 1; DIGITS.contains(string.charAt(newStrLen)); newStrLen--) {
+		}
 
-		for (newStrLen = originalStrLen - 1; digits.contains(string.charAt(newStrLen)); newStrLen--) {
+		return string.substring(0, newStrLen + 1);
+	}
+
+	public static String removeEndRomanetters(String string) {
+		int originalStrLen = string.length();
+		int newStrLen;
+
+		// null string isn't handled
+
+		if (originalStrLen == 0) {
+			return "";
+		}
+
+		for (newStrLen = originalStrLen - 1; ROMAN_LETTERS.contains(string.charAt(newStrLen)); newStrLen--) {
 		}
 
 		return string.substring(0, newStrLen + 1);
