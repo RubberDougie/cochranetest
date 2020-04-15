e88a616de863a43a8604941c3b69b3d8d6bb4110
RubberDougie <48168842+RubberDougie@users.noreply.github.com>
2020-04-15 00:56:40 -0400

    Fixed Xpath again

--- src/test/java/com/rubberdougie/pageobjects/CochraneCDSRReviews.java
+++ src/test/java/com/rubberdougie/pageobjects/CochraneCDSRReviews.java
@@ -35,7 +35,7 @@ public class CochraneCDSRReviews extends PageObject {
 	@FindBy(xpath = "//*[@id=\"column-2\"]/div[1]/div[1]/ul/li[8]/a/span[1]")
 	private WebElement moreTabClickableText;
 
-	@FindBy(xpath = "//*[@id=\"column-2\"]/div[1]/div[1]/ul/li[8]/div/ul/li[1]/a/span[1]")
+	@FindBy(xpath = "//*[@id=\"column-2\"]/div[1]/div[1]/ul/li[8]/div/ul/li[3]/a/span[1]")
 	private WebElement clinicalAnswersClickableTextUnderMore; // for 900x900
 
 	@FindBy(xpath = "/html/body/div[1]/div[5]/div[1]/div[2]/div/div/div/div[1]/div/section/div[1]/div/div/div/div[2]/div/div[1]/div[2]/form/div[1]/ul/li/ul/li/span/a")
